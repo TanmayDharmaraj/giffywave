7123c8b708777f93c6aead918a61d872ccae6ea9
Tanmay Dharmaraj <tanmay.dharmaraj@rapidcircle.com>
2017-04-18 11:55:33 +0530

    Added additional logging

--- app/index.js
+++ app/index.js
@@ -4,18 +4,21 @@ const clarify = require('./services/recognition-service');
 const giffy = require('./services/gif-service');
 const image = require('./services/image-helper');
 
+console.log("Encoding image");
 image.EncodeImage("./app/images/food-004.jpg", function(err, base64EncodedImage) {
     if(err){
       console.error(err);
       return;
     }
+    console.log("Predicting food items");
     clarify.PredictFoodItems(base64EncodedImage, function(err, resp) {
+        console.log("Finished predicting food items");
         if (err) {
             console.error(err);
             return;
         }
         var tag = resp[0];
-        console.log("Getting gif for " + tag);
+        console.log("Getting gif for food item : " + tag);
         giffy.GetRandomGif(resp[0], function(err, gif) {
             console.log(gif);
         });
